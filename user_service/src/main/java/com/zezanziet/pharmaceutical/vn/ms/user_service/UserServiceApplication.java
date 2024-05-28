897a725e2676708b8bae5276ad9339cba5495880
Ze Zan Ziet <vietvan05032002@gmail.com>
2024-05-28 13:59:39 +0700

    Create springboot BE project for user_service

---
+++ user_service/src/main/java/com/zezanziet/pharmaceutical/vn/ms/user_service/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zezanziet.pharmaceutical.vn.ms.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
